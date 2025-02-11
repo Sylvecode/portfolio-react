48e2299a1fcff1d636cdd8a88d1d5a4bddb5a4df
Sylvain périé <sylvain.perie@hotmail.fr>
2025-02-11 12:29:57 +0100

    filled pokecodes project page

--- src/pages/PokeCodes.tsx
+++ src/pages/PokeCodes.tsx
@@ -59,10 +59,7 @@ export default function PokeCodes() {
         </div>
         <div className="m-10 max-w-3xl mx-auto bg-white rounded-lg shadow-lg p-8 dark:bg-gray-800/50 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 dark:shadow-none transition-all duration-250 focus:outline focus:outline-2 focus:outline-red-500">
           <div className="flex flex-row flex-wrap justify-center m-2 gap-12">
-            <div className="flex flex-col justify-center align-center items-center">
-              <SiJavascript className="m-3 size-8 text-blue-600" />
-              <p className="text-dark dark:text-white">Javascript</p>
-            </div>
+            <img src="/images/screen_pokecodes3.png" className="rounded"></img>
           </div>
         </div>
       </div>
